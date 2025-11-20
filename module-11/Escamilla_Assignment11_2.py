d85986df97d409b4f2593019cd31e54385bdc33f
Carlos E. Escamilla <Carlose197777@gmail.com>
2025-11-20 16:16:55 -0600

    Added module-11 Assignment: Django

---
+++ module-11/Escamilla_Assignment11_2.py
@@ -0,0 +1,55 @@
+"""
+---
+
+# CSD325-T301 - Week 11.2 Assignment: Django Basics
+
+---
+
+**Professor**: John Woods<br>
+**@Copyright**: BELLEVUE.edu<br>
+**Modified By**: Carlos E. Escamilla<br>
+**Email**: CEEscamilla@my365.BELLEVUE.edu<br>
+**OS**: Windows 11 x64<br>
+**Processor**: i9-13900<br>
+**GPU**: NVIDIA GeForce RTX 3060<br>
+**IDE**: DataSpell 2025.2<br>
+**Interpreter**: Python 3.12<br>
+**Libraries Managed by**: Miniforge3
+
+---
+
+**Version**:
+- 1.0.0 - 2025.09.08 Week1: Programming Logic
+- 1.0.1 - 2025.09.15 Week2: Documenting Debugging
+- 1.0.2 - 2025.09.22 Week3: Brownfield Development
+- 1.0.3 - 2025.09.29 Week4: CSV Read and Matplotlib
+- 1.0.4 - 2025.10.06 Week5: Forest Fire Simulation 1
+- 1.0.5 - 2025.10.13 Week6: Forest Fire Simulation 2
+- 1.0.6 - 2025.10.20 Week7: Test Cases
+- 1.0.7 - 2025.10.27 Week8: JSON Practice
+- 1.0.8 - 2025.11.03 Week9: JSON and Application Programming Interfaces (APIs)
+- 1.0.9 - 2025.11.10 Week10: Tkinter and GUI Forms
+- 1.0.10 - 2025.11.17 Week11: Django Basics
+
+**Assignment**:
+1. Start viewing Learn Django in 20 Minutes!
+2. Created the project, (around minute 3:30) stop and open a Command Line Interface and navigate to your demo directory.
+    Do a directory listing (dir in Windows or ls in iOs) and make sure manage.py is visible.
+    Type in the following: python manage.py runserver.
+    Now, open up a browser window and type in the address 127.0.0.1:8000 and take a screenshot of the page.
+    It should look something like: Decorative
+    Paste the screenshot into the Word document.
+3. Restart the server. In the command line interface, stop the server; CTRL-C, then python manage.py runserver
+4. At about minute 6:50, you'll see text associated with the HttpResponse.
+    Change "hello world" to "<your last name> says Hello!" For example : "Sampson says Hello!"
+5. Go out to your local host and see <your last name>" says Hello!
+    Take a screenshot. Paste that screenshot into your Word document.
+6. Save your Word document to your module-11 directory.
+
+"""
+
+# ==================================================
+# Django Basics
+# ==================================================
+
+
